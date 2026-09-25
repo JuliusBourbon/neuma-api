ce6157372cc7f1da3b53ef9293e865b3baa0be25
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-09-25 13:14:54 +0700

    feat: pass preferred language to onboarding service

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,37 +1,44 @@
-import * as usersService from './users.service.js';
+import * as usersService from "./users.service.js";
 
 export async function getMe(req, res, next) {
-    try {
-        const user = await usersService.getUserProfile(req.userId);
-        res.status(200).json({ success: true, data: { user } });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const user = await usersService.getUserProfile(req.userId);
+    res.status(200).json({ success: true, data: { user } });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function updateMe(req, res, next) {
-    try {
-        const user = await usersService.updateUserProfile(req.userId, req.validated.body);
-        res.status(200).json({ success: true, data: { user } });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const user = await usersService.updateUserProfile(
+      req.userId,
+      req.validated.body,
+    );
+    res.status(200).json({ success: true, data: { user } });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function completeOnboarding(req, res, next) {
-    try {
-        const user = await usersService.completeOnboarding(req.userId);
-        res.status(200).json({ success: true, data: { user } });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const user = await usersService.completeOnboarding(
+      req.userId,
+      req.validated.body.preferredLanguage,
+    );
+
+    res.status(200).json({ success: true, data: { user } });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function getMyStats(req, res, next) {
-    try {
-        const stats = await usersService.getUserStats(req.userId);
-        res.status(200).json({ success: true, data: { stats } });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const stats = await usersService.getUserStats(req.userId);
+    res.status(200).json({ success: true, data: { stats } });
+  } catch (err) {
+    next(err);
+  }
 }
